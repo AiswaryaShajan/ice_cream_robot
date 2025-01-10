bfa67e2d02200960a3cac3593f9641b4aa77d010
Aiswarya Shajan <AS4213@live.mdx.ac.uk>
2025-01-10 21:41:07 +0400

    modified positions to avoid collisions

--- script.py
+++ script.py
@@ -9,111 +9,102 @@ open_gripper_program = RDK.Item('open_gripper', ITEM_TYPE_PROGRAM)  # Opening th
 close_gripper_program = RDK.Item('close_gripper', ITEM_TYPE_PROGRAM)  # Closing the gripper
 attach = RDK.Item('attach', ITEM_TYPE_PROGRAM)  # Attaching to the gripper
 detach = RDK.Item('detach', ITEM_TYPE_PROGRAM)  # Detaching from the gripper
-
-# Teach points defined for home and picking up first cone from input station
 home = RDK.Item('home')
+
+# INPUT STATION - PICKING
 pick_1 = RDK.Item('pick_1')
+pick_2 = RDK.Item('pick_2')
+pick_3 = pick_1.Pose() * transl(0, 0, -120)
+pick_4 = pick_2.Pose() * transl(0, 0, -120)
+pick_5 = pick_1.Pose() * transl(0, 0, -240)
+pick_6 = pick_2.Pose() * transl(0, 0, -240)
+approach_pick = RDK.Item('approach_pick')
 
-# Offset points for picking the remaining cones
-pick_2 = pick_1.Pose() * transl(0, -121, 0)
-pick_3 = pick_1.Pose() * transl(0, 0, -119)
-pick_4 = pick_1.Pose() * transl(0, -121, -119)
-pick_5 = pick_1.Pose() * transl(0, 0, -238)
-pick_6 = pick_1.Pose() * transl(0, -121, -238)
-
-# Offset points for approaching each cone
-approach_1 = pick_1.Pose() * transl(300, 0, 0)
-approach_2 = pick_1.Pose() * transl(300, -121, 0)
-approach_3 = pick_1.Pose() * transl(300, 0, -119)
-approach_4 = pick_1.Pose() * transl(300, -121, -119)
-approach_5 = pick_1.Pose() * transl(300, 0, -238)
-approach_6 = pick_1.Pose() * transl(300, -121, -238)
-
-# Teach points for filling station
-approach_fill = RDK.Item('approach_fill')  # Approach point for filling station
+# FILL STATION (points in order of path)
+approach_fill = RDK.Item('approach_fill')
 enter_fill = RDK.Item('enter_fill')
-fill_1_go_up = RDK.Item('fill_1_go_up')
-fill_1_go_down = fill_1_go_up.Pose() * transl(-15, 0, 0)
-intermediate_fill = RDK.Item('intermediate_fill')
+fill_test_4 = RDK.Item('fill_test_4')
+fill_1_go_down = RDK.Item('fill_1_go_down')
+fill_1_go_up = fill_1_go_down.Pose() * transl(15, 0, 0)
+fill_test_1 = RDK.Item('fill_test_1')
 approach_fill_2 = RDK.Item('approach_fill_2')
-intermediate_fill_2 = RDK.Item('intermediate_fill_2')
+fill_test_2 = RDK.Item('fill_test_2')
 fill_2_go_down = RDK.Item('fill_2_go_down')
-test_1 = RDK.Item('test_1')
-test_2 = RDK.Item('test_2')
+fill_2_go_up = fill_2_go_down.Pose() * transl(15, 0, 0)
 exit_fill = RDK.Item('exit_fill')
+fill_test = RDK.Item('fill_test')
+fill_test_3 = RDK.Item('fill_test_3')
 
-# Teach points for dropping station
-approach_drop_1 = RDK.Item('approach_drop')
+# OUTPUT STATION - DROPPING
+approach_drop = RDK.Item('approach_drop')
 drop_1 = RDK.Item('drop_1')
-approach_drop_2 = drop_1.Pose()*transl(120,77,140)
-drop_2 = drop_1.Pose()*transl(0,77,140)
-approach_drop_3 = drop_1.Pose()*transl(120,227,140)
-drop_3 = drop_1.Pose()*transl(0,227,140)
-approach_drop_4 = drop_1.Pose()*transl(120,300,15)
-drop_4 = drop_1.Pose()*transl(0,300,15)
+drop_2 = RDK.Item('drop_2')
+drop_3 = RDK.Item('drop_3')
+drop_4 = RDK.Item('drop_4')
 drop_5 = RDK.Item('drop_5')
-approach_drop_5 = drop_5.Pose()*transl(120,0,0)
-approach_drop_6 = drop_5.Pose()*transl(120,-150,-5)
-drop_6 = drop_5.Pose()*transl(0,-150,-5)
+drop_6 = RDK.Item('drop_6')
+approach_drop_1 = drop_1.Pose() * transl(100, 0, 0)
+approach_drop_2 = drop_2.Pose() * transl(100, 0, 0)
+approach_drop_3 = drop_3.Pose() * transl(100, 0, 0)
+approach_drop_4 = drop_4.Pose() * transl(100, 0, 0)
+approach_drop_5 = drop_5.Pose() * transl(100, 0, 0)
+approach_drop_6 = drop_6.Pose() * transl(100, 0, 0)
 
-cone_pick_positions = [pick_1, pick_2, pick_3, pick_4, pick_5, pick_6]
-approach_pick_positions = [approach_1, approach_2, approach_3, approach_4, approach_5, approach_6]
-drop_positions = [drop_1, drop_2, drop_3, drop_4, drop_6, drop_5]
-approach_drop_positions = [approach_drop_1, approach_drop_2, approach_drop_3, approach_drop_4, approach_drop_6, approach_drop_5]
+# Lists for convenience
+pick_positions = [pick_1, pick_2, pick_3, pick_4, pick_5, pick_6]
+approach_drop_positions = [approach_drop_1, approach_drop_2, approach_drop_3, approach_drop_4, approach_drop_5, approach_drop_6]
+drop_positions = [drop_1, drop_2, drop_3, drop_4, drop_5, drop_6]
 
 # Loop through each cone position
 for i in range(6):
-    # Move to home position
+    # Move to home position and open gripper
     robot.MoveJ(home)
     open_gripper_program.RunProgram()
     time.sleep(1)
     
     # Move to the approach position and then pick up the cone
-    robot.MoveJ(approach_pick_positions[i])
-    time.sleep(1)
-    robot.MoveJ(cone_pick_positions[i])
+    robot.MoveJ(approach_pick)
+    robot.MoveL(pick_positions[i])
     time.sleep(1)
     close_gripper_program.RunProgram()
     time.sleep(2)
     attach.RunProgram()
     time.sleep(1)
+    robot.MoveL(approach_pick)
     
     # Move to the filling station
-    robot.MoveJ(approach_pick_positions[i])
-    time.sleep(1)
     robot.MoveJ(approach_fill)
-    time.sleep(1)
-    
+    robot.MoveL(enter_fill)
+
     # Lower speed for filling station
-    robot.setSpeed(100)  # Set slower translation speed
-    robot.setSpeedJoints(50)  # Set slower joint speed
-    
+    robot.setSpeed(50)  # Set slower translation speed
+    robot.setSpeedJoints(25)  # Set slower joint speed
+
     # Perform filling operations
-    robot.MoveC(enter_fill, intermediate_fill)
-    robot.MoveL(fill_1_go_down)
-    time.sleep(1)
+    robot.MoveC(fill_test_4, fill_1_go_down)
     robot.MoveL(fill_1_go_up)
-    time.sleep(0.5)
     robot.MoveL(fill_1_go_down)
-    time.sleep(1)
-    robot.MoveC(approach_fill_2, intermediate_fill_2)
-    robot.MoveL(fill_2_go_down)
-    fill_2_go_up = fill_2_go_down.Pose() * transl(25,0,0) 
+    robot.MoveC(fill_test_1, approach_fill_2)
+    robot.MoveC(fill_test_2, fill_2_go_down)
     robot.MoveL(fill_2_go_up)
-    time.sleep(1)
     robot.MoveL(fill_2_go_down)
-    time.sleep(1)
-    robot.MoveL(test_1)
-    robot.MoveL(test_2)
-    robot.MoveL(exit_fill)
-        
+    robot.MoveL(fill_test)
+    robot.MoveC(fill_test_3, exit_fill)
+    
     # Restore original speed settings
     robot.setSpeed(500)  # Restore translation speed
     robot.setSpeedJoints(150)  # Restore joint speed
-    
-    # Drop the cone at the dropping station
-    robot.MoveJ(approach_drop_positions[i])
-    robot.MoveJ(drop_positions[i])
+
+    # Move to the dropping station
+    robot.MoveL(approach_drop)
+    robot.MoveL(approach_drop_positions[i])
+    robot.MoveL(drop_positions[i])
     detach.RunProgram()
     time.sleep(1)
-    robot.MoveJ(home)
+    open_gripper_program.RunProgram()  # Open gripper after dropping
+    time.sleep(2)
+    robot.MoveL(approach_drop_positions[i])  # Go back to approach position
+
+# Finish by moving to home position
+time.sleep(1)
+robot.MoveJ(home)
